3b72550a354e419efb358119a9e4eebb59026457
Nikita Orlov <zunderzytgg@mail.ru>
2024-09-28 10:32:14 +0300

    создание основной логики

---
+++ Rock-Paper-ScissorsWPF/Game.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rock_Paper_ScissorsWPF
+{
+    public enum Move { Stone, Paper, Scissors }
+
+    public class Game
+    {
+        private Random random = new Random();
+
+        public Move GetComputerMove()
+        {
+            return (Move)random.Next(0, 3);
+        }
+
+        public string GetResult(Move playerMove, Move computerMove)
+        {
+            switch (playerMove)
+            {
+                case Move.Stone:
+                    switch (computerMove)
+                    {
+                        case Move.Stone:
+                            return "Ничья!";
+                        case Move.Scissors:
+                            return "Вы выиграли!";
+                        case Move.Paper:
+                            return "Компьютер выиграл!";
+                    }
+                    break;
+
+                case Move.Paper:
+                    switch (computerMove)
+                    {
+                        case Move.Stone:
+                            return "Вы выиграли!";
+                        case Move.Scissors:
+                            return "Компьютер выиграл!";
+                        case Move.Paper:
+                            return "Ничья!";
+                    }
+                    break;
+
+                case Move.Scissors:
+                    switch (computerMove)
+                    {
+                        case Move.Stone:
+                            return "Компьютер выиграл!";
+                        case Move.Scissors:
+                            return "Ничья!";
+                        case Move.Paper:
+                            return "Вы выиграли!";
+                    }
+                    break;
+            }
+            return "";
+        }
+    }
+}
--- Rock-Paper-ScissorsWPF/MainWindow.xaml.cs
+++ Rock-Paper-ScissorsWPF/MainWindow.xaml.cs
@@ -20,9 +20,62 @@ namespace Rock_Paper_ScissorsWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        private int playerScore = 0;
+        private int computerScore = 0;
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void bt_start_Click(object sender, RoutedEventArgs e)
+        {
+            bt_start.Visibility = Visibility.Hidden;
+            bt_Paper.Visibility = Visibility.Visible;
+            bt_Stone.Visibility = Visibility.Visible;
+            bt_Scissors.Visibility = Visibility.Visible;
+        }
+        private void UpdateScore(string result)
+        {
+            if (result == "Вы выиграли!")
+            {
+                playerScore++;
+                lb_you.Content = $"{playerScore}";
+            }
+            else if (result == "Компьютер выиграл!")
+            {
+                computerScore++;
+                lb_ii.Content = $"{computerScore}";
+            }
+        }
+
+        private void bt_Stone_Click(object sender, RoutedEventArgs e)
+        {
+            Game game = new Game();
+            Move playerMove = Move.Stone;
+            Move computerMove = game.GetComputerMove();
+            string result = game.GetResult(playerMove, computerMove);
+            UpdateScore(result);
+            MessageBox.Show(result);
+        }
+
+        private void bt_Scissors_Click(object sender, RoutedEventArgs e)
+        {
+            Game game = new Game();
+            Move playerMove = Move.Scissors;
+            Move computerMove = game.GetComputerMove();
+            string result = game.GetResult(playerMove, computerMove);
+            UpdateScore(result);
+            MessageBox.Show(result);
+        }
+
+        private void bt_Paper_Click(object sender, RoutedEventArgs e)
+        {
+            Game game = new Game();
+            Move playerMove = Move.Paper;
+            Move computerMove = game.GetComputerMove();
+            string result = game.GetResult(playerMove, computerMove);
+            UpdateScore(result);
+            MessageBox.Show(result);
         }
     }
 }
